3ad3171931486e5255baae658679709da46b3af7
shit <pedrocunha@alunos.utfpr.edu.br>
2020-03-27 20:53:06 -0300

    Primeiro código de manipulacão de arquivos

---
+++ Files/openfile.c
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*
+Este programa demonstra o funcionamento da Syscall do Linux "open()".
+Essa Syscall abre um arquivo ou o cria caso não haja um.
+< 0 se ocorre um erro
+== 0 se o processo é o processo filho que acabou de ser criado
+
+
+O programa chama a função "open()" que abre um arquivo e escreve "Hello World" e em seguida o fecha.
+*/
+
+int main(int argc, char const *argv[])
+{
+	int fd;
+
+	fd=open("TextFile",2);
+	
+		write(fd,stdin,14);
+		close(3);
+	return 0;
+}
