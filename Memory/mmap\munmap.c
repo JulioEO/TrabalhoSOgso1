ec032ae3a1fb283279089147ad1048761d9149ff
leo-nar <40218270+leo-nar@users.noreply.github.com>
2020-03-28 20:18:40 -0300

    novas explicacoes

--- Memory/mmap\munmap.c
+++ Memory/mmap\munmap.c
@@ -7,14 +7,19 @@
 	/*mmap() e uma funcao que mapeia parte da memoria 
 		e define quem tem acesso a ela*/
 	/* munmap() e uma funcao que desmapeia uma parte da memoria*/
+	/*nesse exemplo iremos mapear uma regiao da memoria escrever
+	uma string nela ler essa regiao e demapeala*/
 int main(){
 
 	size_t pagesize = getpagesize();
 
 	char *p;
 
-	p = mmap(sbrk(0),pagesize*5,
-    PROT_READ|PROT_WRITE|PROT_EXEC,MAP_ANON|MAP_PRIVATE,0,0);
+	p = mmap(sbrk(0)//pega um lugar endereço igual ou maior ao progan break
+		,pagesize*5,
+    		PROT_READ|PROT_WRITE|PROT_EXEC//o espaço pode ser lido, escrito,executad
+		 ,MAP_ANON|MAP_PRIVATE//
+		 ,0,0);
 
     strcpy(p, "Empoleon Nº395\nIt swims as fast as a jet boat. The edges of its wings are sharp and can slice apart drifting ice.");
 
